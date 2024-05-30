ca7afba8ac9e0ae016e8cda6fab1572d44e1480a
marianodilup <mangypul@gmail.com>
2024-05-30 22:10:00 +0200

    components and pages

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieFormPageComponent } from './pages/movie-form-page/movie-form-page.component';
+
+export const routes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'movies',component:MovieListComponent},
+    {path:'create', component:MovieFormPageComponent}
+];
